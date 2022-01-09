9a956afd74d6bc7c551439b393e023e4ba6386a3
Allan Lang <allan@MacBookPro-13.local>
2022-01-09 21:02:55 +0000

    Date formatter to handle fractional seconds

--- Tree Tracker/Extensions/JSONDecoder.swift
+++ Tree Tracker/Extensions/JSONDecoder.swift
@@ -3,7 +3,11 @@ import Foundation
 extension JSONDecoder {
     static var _iso8601ms: JSONDecoder {
         let decoder = JSONDecoder()
-        decoder.dateDecodingStrategy = .formatted(._iso8601ms)
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US_POSIX")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+        formatter.timeZone = TimeZone(secondsFromGMT: 0)
+        decoder.dateDecodingStrategy = .formatted(formatter)
 
         return decoder
     }
